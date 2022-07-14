420d6a3212055c9fec3a537abb171c5c09470ed4
d <david.kozak@gmx.com>
2022-07-14 22:46:05 +0200

    first

---
+++ src/main/java/the/david/restartqueue/command/ConstructTabCompleter.java
@@ -0,0 +1,34 @@
+package the.david.restartqueue.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import the.david.restartqueue.Restartqueue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConstructTabCompleter implements TabCompleter {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+        List<String> list = new ArrayList<>();
+        if (args.length == 1) {
+            list.add("add");
+            list.add("remove");
+            list.add("list");
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("remove")) {
+            JSONArray json = Restartqueue.storadge.getArrayFromFile();
+            for (Object o : json) {
+                JSONObject obj = (JSONObject) o;
+                if (player.getUniqueId().toString().equalsIgnoreCase((String) obj.get("creator"))) {
+                    list.add((String) obj.get("name"));
+                }
+            }
+        }
+        return list;
+    }
+}
