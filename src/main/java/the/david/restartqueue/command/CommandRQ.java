a2224c2fb5806ec54298a20fcbbf56e93b420c35
D0A1V2I3D <AoatO34n3>
2022-07-15 10:04:39 +0200

    import cleanup

--- src/main/java/the/david/restartqueue/command/CommandRQ.java
+++ src/main/java/the/david/restartqueue/command/CommandRQ.java
@@ -1,8 +1,5 @@
 package the.david.restartqueue.command;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
@@ -13,29 +10,30 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import the.david.restartqueue.Restartqueue;
 
-import static org.bukkit.Bukkit.getLogger;
+import java.util.UUID;
 
 public class CommandRQ implements CommandExecutor {
+    @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 1) {
-            Player player = (Player)sender;
+        if (args.length == 1){
+            Player player = (Player) sender;
             if (args[0].equalsIgnoreCase("list")) {
                 JSONArray json = Restartqueue.storadge.getArrayFromFile();
                 for (Object o : json) {
-                    JSONObject obj = (JSONObject)o;
-                    if (player.getUniqueId().toString().equalsIgnoreCase(obj.get("creator").toString()))
+                    JSONObject obj = (JSONObject) o;
+                    if (player.getUniqueId().toString().equalsIgnoreCase(obj.get("creator").toString())) {
                         player.sendMessage("Name: " + obj.get("name").toString());
+                    }
                 }
                 return true;
             }
-        } else if (args.length != 2) {
+        }else if (args.length != 2) {
             return false;
         }
         if (sender instanceof Player) {
-            Player player = (Player)sender;
+            Player player = (Player) sender;
             if (args[0].equalsIgnoreCase("add")) {
                 Block block = player.getTargetBlock(null, 5);
-                getLogger().info(player.getUniqueId().toString());
                 if (block.getType() == Material.LEVER) {
                     UUID uuid = player.getUniqueId();
                     Restartqueue.storadge.addLever(block, args[1], uuid.toString());
@@ -43,34 +41,19 @@ public class CommandRQ implements CommandExecutor {
                     return false;
                 }
                 return true;
-            }
-            if (args[0].equalsIgnoreCase("remove")) {
+            } else if (args[0].equalsIgnoreCase("remove")) {
                 JSONArray json = Restartqueue.storadge.getArrayFromFile();
                 for (Object o : json) {
-                    JSONObject obj = (JSONObject)o;
-                    if (player.getUniqueId().toString().equalsIgnoreCase(obj.get("creator").toString()) &&
-                            args[1].equalsIgnoreCase(obj.get("name").toString())) {
-                        json.remove(o);
-                        Restartqueue.storadge.writeFile(json);
-                        return true;
+                    JSONObject obj = (JSONObject) o;
+                    if (player.getUniqueId().toString().equalsIgnoreCase(obj.get("creator").toString())) {
+                        if (args[1].equalsIgnoreCase(obj.get("name").toString())) {
+                            json.remove(o);
+                            Restartqueue.storadge.writeFile(json);
+                            return true;
+                        }
                     }
                 }
                 return false;
-            }
-            if (args[0].equalsIgnoreCase("location")) {
-                JSONArray json = Restartqueue.storadge.getArrayFromFile();
-                for (Object o : json) {
-                    JSONObject obj = (JSONObject)o;
-                    if (player.getUniqueId().toString().equalsIgnoreCase(obj.get("creator").toString()) &&
-                            args[1].equalsIgnoreCase(obj.get("name").toString())) {
-                        List<String> location = new ArrayList<>();
-                        JSONArray loc = (JSONArray)obj.get("location");
-                        for (int i = 0; i < 3; i++)
-                            location.add(loc.get(i).toString());
-                        player.sendMessage("XYZ: " + (String)location.get(0) + " " + (String)location.get(1) + " " + (String)location.get(2));
-                        return true;
-                    }
-                }
             }
         }
         return false;
--- src/main/java/the/david/restartqueue/db/LeverStoradge.java
+++ src/main/java/the/david/restartqueue/db/LeverStoradge.java
@@ -1,5 +1,11 @@
 package the.david.restartqueue.db;
 
+import org.bukkit.block.Block;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,25 +14,21 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import org.bukkit.block.Block;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 public class LeverStoradge {
     private File file;
-
     private JSONArray json;
-
     private JSONParser parser = new JSONParser();
 
     public LeverStoradge() {
-        if (!Files.exists(Paths.get("plugins/restartqueue", new String[0]), new java.nio.file.LinkOption[0]))
-            (new File("plugins/restartqueue")).mkdirs();
-        this.file = new File("plugins/restartqueue/levers.json");
-        if (!Files.exists(Paths.get("plugins/restartqueue/levers.json", new String[0]), new java.nio.file.LinkOption[0]))
+        if (!Files.exists(Paths.get("plugins/restartqueue"))) {
+            new File("plugins/restartqueue").mkdirs();
+        }
+        file = new File("plugins/restartqueue/levers.json");
+        if (!Files.exists(Paths.get("plugins/restartqueue/levers.json"))) {
             try {
-                this.file.createNewFile();
+                file.createNewFile();
+
                 FileWriter fw = new FileWriter("plugins/restartqueue/levers.json");
                 fw.write("[]");
                 fw.close();
@@ -34,9 +36,10 @@ public class LeverStoradge {
                 System.out.println("Critical error occurred when trying to create a file to store lever data in.");
                 e.printStackTrace();
             }
+        }
         try {
-            this.json = (JSONArray)this.parser.parse(new FileReader("plugins/restartqueue/levers.json"));
-        } catch (IOException|org.json.simple.parser.ParseException e) {
+            json = (JSONArray) parser.parse(new FileReader("plugins/restartqueue/levers.json"));
+        } catch (IOException | ParseException e) {
             System.out.println("Critical errors occured when trying to read levers.json");
             e.printStackTrace();
         }
@@ -45,7 +48,7 @@ public class LeverStoradge {
     public void addLever(Block lever, String name, String uuid) {
         JSONObject obj = new JSONObject();
         obj.put("name", name);
-        obj.put("todo", Integer.valueOf(0));
+        obj.put("todo", 0);
         obj.put("creator", uuid);
         obj.put("world_name", lever.getWorld().getName());
         List<String> location = new ArrayList<>();
@@ -54,35 +57,34 @@ public class LeverStoradge {
         location.add(String.valueOf(lever.getZ()));
         obj.put("location", location);
         try {
-            this.json = (JSONArray)this.parser.parse(new FileReader("plugins/restartqueue/levers.json"));
-        } catch (IOException|org.json.simple.parser.ParseException e) {
+            json = (JSONArray) parser.parse(new FileReader("plugins/restartqueue/levers.json"));
+        } catch (IOException | ParseException e) {
             System.out.println("Critical errors occured when trying to read levers.json");
             e.printStackTrace();
         }
-        this.json.add(obj);
+        json.add(obj);
         try {
-            FileWriter fw = new FileWriter(this.file);
-            fw.write(this.json.toJSONString());
+            FileWriter fw = new FileWriter(file);
+            fw.write(json.toJSONString());
             fw.close();
         } catch (IOException e) {
             System.out.println("Critical errors occured when trying to write to levers.json");
             e.printStackTrace();
         }
     }
-
     public JSONArray getArrayFromFile() {
         try {
-            this.json = (JSONArray)this.parser.parse(new FileReader("plugins/restartqueue/levers.json"));
-        } catch (IOException|org.json.simple.parser.ParseException e) {
+            json = (JSONArray) parser.parse(new FileReader("plugins/restartqueue/levers.json"));
+        } catch (IOException | ParseException e) {
             System.out.println("Critical errors occured when trying to read levers.json");
             e.printStackTrace();
         }
-        return this.json;
+        return json;
     }
 
     public void writeFile(JSONArray json) {
         try {
-            FileWriter fw = new FileWriter(this.file);
+            FileWriter fw = new FileWriter(file);
             fw.write(json.toJSONString());
             fw.close();
         } catch (IOException e) {
